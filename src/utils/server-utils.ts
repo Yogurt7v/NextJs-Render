4c73816f18c2da9ee1623b0b7828530e06aaa3b5
Yogurt7v <egorov-2k@yandex.ru>
2024-06-08 11:30:17 +0300

    client/server route

---
+++ src/utils/server-utils.ts
@@ -0,0 +1,9 @@
+export const serverSideFunction = () => {
+    console.log(
+        `use multiple libraries,
+        use environment variables,
+        interact with a databases,
+        process confidential data`
+    )
+    return "server result in console"
+}
